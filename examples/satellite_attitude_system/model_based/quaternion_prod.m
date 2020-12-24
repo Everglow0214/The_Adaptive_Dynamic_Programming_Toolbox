4e41a43ed98ed594e029d206e05060bab6b694ab
Xiaowei Xing <997427575@qq.com>
2020-12-24 20:05:07 +0900

    Add files via upload

---
+++ examples/satellite_attitude_system/model_based/quaternion_prod.m
@@ -0,0 +1,12 @@
+function p = quaternion_prod(q,r)
+% Quaternion product.
+
+if numel(r) == 3
+    r = [0; r];
+end
+q_matrix = [q(1) -q(2) -q(3) -q(4);
+            q(2) q(1) -q(4) q(3);
+            q(3) q(4) q(1) -q(2);
+            q(4) -q(3) q(2) q(1)];
+p = q_matrix * r;
+end
